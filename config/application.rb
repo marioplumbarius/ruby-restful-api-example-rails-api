245ce94e74d2d3810a2807ff3b9facbc1453ee56
marioluan <mario.souza@totvs.com.br>
2016-09-23 11:06:23 -0400

    removed Rack::Sendfile middleware

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # List of middlewares that we don't need
+    config.middleware.delete ::Rack::Sendfile
   end
 end
