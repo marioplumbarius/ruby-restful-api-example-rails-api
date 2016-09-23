a4476f1a71867d800625cd7112566391c469dab1
marioluan <mario.souza@totvs.com.br>
2016-09-23 11:32:36 -0400

    Revert "removed ActiveSupport::Cache::Strategy::LocalCache::Middleware middleware"
    This reverts commit 0ba8af62afbea9d464722a1c9fb91128f54fc0c3.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,14 +12,16 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Show full error reports.
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+  config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
