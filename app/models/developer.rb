d10416b3e9ed08468104e2df7657686cc5cd2f79
marioluan <mario.souza@totvs.com.br>
2016-09-26 10:26:16 -0400

    added validation to developer's name and age fields

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,4 @@
 class Developer < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 250 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 1, less_than_or_equal_to: 150 }
 end
