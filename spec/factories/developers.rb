e948145f96629b08fec2ccd5a61683a5af206525
marioluan <mario.souza@totvs.com.br>
2016-09-27 18:37:55 -0400

    fixed range for Developer fixture

--- spec/factories/developers.rb
+++ spec/factories/developers.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :developer do
     name { Faker::Name.name }
-    age { Faker::Number.between(1, 100) }
+    age { Faker::Number.between(2, 100) }
 
     trait :invalid do
       name { "" }
-      age { Faker::Number.between(-100, -1) }
+      age { Faker::Number.between(-100, -2) }
     end
   end
 end
