49baab85a3897579c579c73517030db583793306
marioluan <mario.souza@totvs.com.br>
2016-09-27 09:23:00 -0400

    removed nonexistent routes from DevelopersController's routing spec

--- spec/routing/developers_routing_spec.rb
+++ spec/routing/developers_routing_spec.rb
@@ -7,16 +7,8 @@ RSpec.describe DevelopersController, type: :routing do
       expect(:get => "/developers").to route_to("developers#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/developers/new").to route_to("developers#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/developers/1").to route_to("developers#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/developers/1/edit").to route_to("developers#edit", :id => "1")
     end
 
     it "routes to #create" do
