9b1f717ab4293bc43bc9973018600df3c21520a2
marioluan <mario.souza@totvs.com.br>
2016-09-27 16:53:04 -0400

    removed dead code from config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,13 +10,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
