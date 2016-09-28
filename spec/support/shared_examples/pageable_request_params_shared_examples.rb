f79a29b7e6f1a0f15572df4fe674c0e57b29376f
marioluan <mario.souza@totvs.com.br>
2016-09-28 10:23:59 -0400

    added shared_examples for pageagle requests' parameters

---
+++ spec/support/shared_examples/pageable_request_params_shared_examples.rb
@@ -0,0 +1,48 @@
+RSpec.shared_examples_for "controller with pageable request params" do
+
+  context "with :page param" do
+    context "when it is set" do
+      let(:page){ Faker::Number.between(1, 3) }
+      let(:params){ {page: page} }
+
+      it "does not set a default value" do
+        get :index, params: params
+
+        expect(subject.params[:page].to_i).to eq page
+      end
+    end
+
+    context "when it is not set" do
+      let(:params){ nil }
+
+      it "sets a default value" do
+        get :index, params: params
+
+        expect(subject.params[:page].to_i).to eq 0
+      end
+    end
+  end
+
+  context "with :per_page param" do
+    context "when it is set" do
+      let(:per_page){ Faker::Number.between(1, 3) }
+      let(:params){ {per_page: per_page} }
+
+      it "does not set a default value" do
+        get :index, params: params
+
+        expect(subject.params[:per_page].to_i).to eq per_page
+      end
+    end
+
+    context "when it is not set" do
+      let(:params){ nil }
+
+      it "sets a default value" do
+        get :index, params: params
+
+        expect(subject.params[:per_page].to_i).to eq Kaminari.config.default_per_page
+      end
+    end
+  end
+end
