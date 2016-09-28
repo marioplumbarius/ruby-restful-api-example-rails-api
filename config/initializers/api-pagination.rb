11b0d1b256dda7fbb591a2a168b1bfd60c510317
marioluan <mario.souza@totvs.com.br>
2016-09-28 14:37:04 -0400

    removed TODOs from source code

--- config/initializers/api-pagination.rb
+++ config/initializers/api-pagination.rb
@@ -1,5 +1,4 @@
 ApiPagination.configure do |config|
-  # TODO - read configuration from environment variables
   config.paginator = :kaminari
   config.total_header = 'X-Pagination-Total'
   config.per_page_header = 'X-Pagination-Per-Page'
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,5 +1,4 @@
 Kaminari.configure do |config|
-  # TODO: read from environment variables
   config.default_per_page = 10
   config.max_per_page = 20
   config.page_method_name = :page
