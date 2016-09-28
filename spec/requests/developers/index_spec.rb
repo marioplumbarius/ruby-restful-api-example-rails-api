64a23fc7ba80aa25c7807806175f594cd0b90bae
marioluan <mario.souza@totvs.com.br>
2016-09-28 14:13:02 -0400

    added specs for GET /developers with query parameters (:name, :age)

--- spec/requests/developers/index_spec.rb
+++ spec/requests/developers/index_spec.rb
@@ -18,8 +18,84 @@ RSpec.describe "Developers", type: :request do
       expect(response).to have_http_status 200
     end
 
-    context "with response body" do
+    context "with params" do
+      context "with :name" do
+        context "when there are developers found" do
+          let(:developers){ create_list :developer, Faker::Number.between(1, 3) }
+          let(:name) { developers.first.name }
+          let(:params){ {name: name} }
+
+          before do
+            get developers_path, params: params
+          end
+
+          it "returns the developers with :name" do
+            items = JSON.parse response.body
+
+            items.each do |item|
+              expect(item["name"]).to eq name
+            end
+          end
+
+        end
+
+        context "when no developer is found" do
+          let(:developers) { create_list :developer, Faker::Number.between(1, 3) }
+          let(:name) { Faker::Name.name }
+          let(:params){ {name: name} }
+
+          before do
+            get developers_path, params: params
+          end
+
+          it "returns an empty body" do
+            expect(JSON.parse(response.body)).to be_empty
+          end
+        end
+      end
+
+      context "with :age" do
+        context "when there are developers found" do
+          let(:developers){ create_list :developer, Faker::Number.between(1, 3) }
+          let(:age) { developers.first.age }
+          let(:params){ {age: age} }
+
+          before do
+            get developers_path, params: params
+          end
+
+          it "returns the developers with :age" do
+            items = JSON.parse response.body
+
+            items.each do |item|
+              expect(item["age"]).to eq age
+            end
+          end
+
+        end
+
+        context "when no developer is found" do
+          let(:developers) { create_list :developer, Faker::Number.between(1, 3) }
+          let(:age) { Faker::Number.digit }
+          let(:params){ {age: age} }
+
+          before do
+            get developers_path, params: params
+          end
+
+          it "returns an empty body" do
+            expect(JSON.parse(response.body)).to be_empty
+          end
+        end
+      end
+    end
+
+    context "without params" do
       context "when no developer is found" do
+        before do
+          get developers_path
+        end
+
         it "returns an empty body" do
           expect(JSON.parse(response.body)).to be_empty
         end
