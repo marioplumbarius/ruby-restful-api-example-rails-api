b276816ff5a7c0b60b1c9b878113f53d97247a2f
marioluan <mario.souza@totvs.com.br>
2016-09-28 12:05:03 -0400

    replaced hard-coded integer by Faker::Number.digit at spec/routing/developers_routing_spec

--- spec/routing/developers_routing_spec.rb
+++ spec/routing/developers_routing_spec.rb
@@ -2,13 +2,14 @@ require "rails_helper"
 
 RSpec.describe DevelopersController, type: :routing do
   describe "routing" do
+    let(:id){ Faker::Number.digit }
 
     it "routes to #index" do
       expect(:get => "/developers").to route_to("developers#index")
     end
 
     it "routes to #show" do
-      expect(:get => "/developers/1").to route_to("developers#show", :id => "1")
+      expect(:get => "/developers/#{id}").to route_to("developers#show", :id => id)
     end
 
     it "routes to #create" do
@@ -16,15 +17,15 @@ RSpec.describe DevelopersController, type: :routing do
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/developers/1").to route_to("developers#update", :id => "1")
+      expect(:put => "/developers/#{id}").to route_to("developers#update", :id => id)
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/developers/1").to route_to("developers#update", :id => "1")
+      expect(:patch => "/developers/#{id}").to route_to("developers#update", :id => id)
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/developers/1").to route_to("developers#destroy", :id => "1")
+      expect(:delete => "/developers/#{id}").to route_to("developers#destroy", :id => id)
     end
 
   end
