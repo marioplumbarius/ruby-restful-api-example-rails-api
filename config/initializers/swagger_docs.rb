c5db94e37b2fbd6bbb6e277f07c04092da337828
marioluan <mario.souza@totvs.com.br>
2016-09-29 11:14:48 -0400

    rubocop: fixed errors for config/initializers/swagger_docs

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -1,25 +1,25 @@
 Swagger::Docs::Config.base_api_controller = ActionController::API
 
-Swagger::Docs::Config.register_apis({
-  "1.0" => {
+Swagger::Docs::Config.register_apis(
+  '1.0' => {
     # the extension used for the API
-    :api_extension_type => :json,
+    api_extension_type: :json,
     # the output location where your .json files are written to
-    :api_file_path => "public/docs",
+    api_file_path: 'public/docs',
     # the URL base path to your API
-    :base_path => "http://localhost",
+    base_path: 'http://localhost',
     # if you want to delete all .json files at each generation
-    :clean_directory => true,
+    clean_directory: true,
     # add custom attributes to api-docs
-    :attributes => {
-      :info => {
-        "title" => "Swagger Sample App",
-        "description" => "This is a sample description.",
-        "termsOfServiceUrl" => "http://helloreverb.com/terms/",
-        "contact" => "apiteam@wordnik.com",
-        "license" => "Apache 2.0",
-        "licenseUrl" => "http://www.apache.org/licenses/LICENSE-2.0.html"
+    attributes: {
+      info: {
+        'title' => 'Swagger Sample App',
+        'description' => 'This is a sample description.',
+        'termsOfServiceUrl' => 'http://helloreverb.com/terms/',
+        'contact' => 'apiteam@wordnik.com',
+        'license' => 'Apache 2.0',
+        'licenseUrl' => 'http://www.apache.org/licenses/LICENSE-2.0.html'
       }
     }
   }
-})
+)
